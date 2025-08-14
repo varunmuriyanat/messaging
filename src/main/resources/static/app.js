900c72a7ae539c1a32283cfad5a6bf605a2f106c
varunmuriyanat <varunmuriyanat@gmail.com>
2025-08-14 16:23:59 -0400

    updated websocket path

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,5 +1,5 @@
 const stompClient = new StompJs.Client({
-    brokerURL: 'ws://localhost:8080/gs-guide-websocket'
+    brokerURL: 'ws://messaging-f5ji.onrender.com/gs-guide-websocket'
 });
 
 stompClient.onConnect = (frame) => {
