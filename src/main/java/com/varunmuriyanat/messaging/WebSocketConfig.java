729c564327024704e78fa47cd116d0c4e6d10e4e
varunmuriyanat <varunmuriyanat@gmail.com>
2025-08-14 13:01:29 -0400

    updated stomp endpoint

--- src/main/java/com/varunmuriyanat/messaging/WebSocketConfig.java
+++ src/main/java/com/varunmuriyanat/messaging/WebSocketConfig.java
@@ -5,20 +5,19 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.lang.NonNull;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-  @Override
-  public void configureMessageBroker(MessageBrokerRegistry config) {
+  public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
     config.enableSimpleBroker("/topic");
     config.setApplicationDestinationPrefixes("/app");
-  }
+  }  
 
   @Override
-  public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/gs-guide-websocket");
+  public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
+    registry.addEndpoint("/my-websocket");
   }
-
 }
